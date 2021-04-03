0974851de563550c245998730ea1c91fc6e03e1c
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-04-03 13:50:28 +0800

    fix responsiveness for madrasah-stack icons

--- components/Home/Hero/madrasah-stack.tsx
+++ components/Home/Hero/madrasah-stack.tsx
@@ -4,21 +4,25 @@ import {allMadrasahs} from '../../../utils/madrasah'
 export default function MadrasahStack() {
 	const renderSingleMadrasah = () => {
 		return allMadrasahs.map(madrasah => (
-			<li
-				className={`inline-block px-1 bg-gray-100 shadow pt-1 border border-opacity-50 rounded-full ${madrasah.borderColor}`}
-				key={madrasah.name}
-			>
-				<Image
-					src={madrasah.iconUrl}
-					alt={madrasah.name}
-					width={40}
-					height={40}
-				/>
-			</li>
+			<div>
+				<li
+					className={`inline-block px-1 bg-gray-100 shadow pt-1 border border-opacity-50 rounded-full ${madrasah.borderColor}`}
+					key={madrasah.name}
+				>
+					<Image
+						src={madrasah.iconUrl}
+						alt={madrasah.name}
+						width={40}
+						height={40}
+					/>
+				</li>
+			</div>
 		))
 	}
 
 	return (
-		<ul className="max-w-xl mx-auto space-x-4">{renderSingleMadrasah()}</ul>
+		<ul className="max-w-xs md:max-w-md mx-auto grid grid-cols-3 gap-y-4 md:grid-cols-6">
+			{renderSingleMadrasah()}
+		</ul>
 	)
 }
